76d363f8920d20142226c30741b342eafbc7d0ac
Fisher <Fisher@Fishers-MBP.ptd.net>
2018-03-23 18:19:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const {prefix,token} = require('./config.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+  if(message.content === `${prefix}ping`){
+    message.channel.send('Pong!');
+  }
+});
+
+client.login(token);
